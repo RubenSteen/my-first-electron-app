27aa82dd2c26acd4f2390ab37d34b84a03e5f6e9
Your Name <you@example.com>
2021-12-16 21:01:37 +0100

    Have to figure this out

--- src/index.js
+++ src/index.js
@@ -9,35 +9,15 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 
 // Creating app window
 const createWindow = () => {
-  // Create the browser window.
-  var frame = false
-
-  if (process.env.APP_ENV === 'local') {
-    frame = true
-  }
 
   const mainWindow = new BrowserWindow({
-    show: false,
-    frame: frame, // Makes the application fullscreen without close buttons
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
-      nodeIntegration: true,
-      contextIsolation: false,
+      // nodeIntegration: true,
+      // contextIsolation: false,
     }
   });
 
-  if (process.env.APP_ENV === 'local') {
-    // Open the DevTools.
-    mainWindow.webContents.openDevTools();
-  } else {
-    // Hide the menubar
-    mainWindow.setAutoHideMenuBar(true)
-  }
-
-  
-
-  // Maximize the window
-  mainWindow.maximize()
+  mainWindow.webContents.openDevTools();
 
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
@@ -70,8 +50,8 @@ app.on('activate', () => {
 
 // Require phidget (https://www.phidgets.com/)
 // Using the module : https://www.phidgets.com/?tier=3&catid=2&pcid=1&prodid=1019 (1202_2 - PhidgetInterfaceKit 0/16/16)
+// Code example : https://www.phidgets.com/?tier=3&catid=2&pcid=1&prodid=1019
 var phidget22 = require('phidget22');
-const { exit } = require('process');
 
 // Create connection to Phidget
 var conn = new phidget22.Connection(parseInt(process.env.PHIDGET_PORT), process.env.PHIDGET_HOST);
@@ -81,144 +61,24 @@ conn.connect().then(function(data) {
 	console.error(`Error during connecting to phidget at ${process.env.PHIDGET_HOST}:${process.env.PHIDGET_PORT}`, err);
 });
 
-let registeredPhidgets = [];
+ //Create your Phidget channels
+ var digitalOutput15 = new phidget22.DigitalOutput();
 
+ //Set addressing parameters to specify which channel to open (if any)
+ digitalOutput15.setDeviceSerialNumber(312483);
+ digitalOutput15.setChannel(15);
 
-// Register the whitelisted phidgets that are passed from the renderer process
-ipcMain.on('register-phidgets', (event, projects) => {
+ //Assign any event handlers you need before calling open so that no events are missed.
 
+ //Open your Phidgets and wait for attachment
+ digitalOutput15.open(5000).then(function() {
 
-  // Register the phidget to variable registeredPhidgets if it has detected it
-  for (projectKey in projects) {
-    projectPhidget = projects[projectKey]['phidget'];
-    for (phidgetSerial in projectPhidget) {
-      for (phidgetChannel in projectPhidget[phidgetSerial]) {
-        let channel = projectPhidget[phidgetSerial][phidgetChannel];
+   //Do stuff with your Phidgets here or in your event handlers.
+   digitalOutput15.setDutyCycle(1);
 
-
-        // This is still creating duplicates, which bring some errors.
-        /*  
-          name: 'PhidgetError',
-          errorCode: 3,
-          message: 'Open timed out'
-        */
-        registeredPhidgets.push(new PhidgetLight(phidgetSerial, channel));
-
-        
-      }
-    }
-  } 
-  
-  // Return some data to the renderer process with the mainprocess-response ID
-  // event.sender.send('register-phidgets-response', registeredPhidgets);
-});
-
-
-// Function is getting called from renderer.js
-ipcMain.on('turn-on-lights', (event, data) => {
-
-  // Since we only want the new lights to be lit, we deactivate the other lights first
-  deactivateLights();
-
-  activateLights(data.phidget);
-
-  // Return some data to the renderer process with the mainprocess-response ID
-  event.sender.send('mainprocess-response', data);
-});
-
-ipcMain.on('turn-off-lights', (event, data) => {
-
-  // Finally I want this function to turn off all the active channels.
-  registeredPhidgets
-      .filter(value => value.isActivated())
-      .forEach(phidgetLight => phidgetLight.deactivate());
-
-});
-
-function activateLights(data) {
-  // Filter out the PhidgetLight instances that now should be activated by serial and channel
-
-  // // Looping over the phidget data that is being send by the renderer process
-  for (const serial in data) {
-
-    for (const key in data[serial]) {
-
-      let channel = data[serial][key];
-
-      registeredPhidgets
-      .filter(value => value.getSerial() === serial && value.getChannel() === channel)
-      // and activate each
-      .forEach(phidgetLight => phidgetLight.activate());
-      
-      
-      
-    }
-  }
-}
-
-function deactivateLights() {
-  // Filter out the activated PhidgetLight instances
-  registeredPhidgets
-      .filter(value => value.isActivated())
-      // and deactivate
-      .forEach(phidgetLight => phidgetLight.deactivate());
-}
-
-class PhidgetLight {
-  constructor(serial, channel) {
-    this.serial = serial;
-    this.channel = channel;
-    this.digitalOutput = new phidget22.DigitalOutput();
-    this.digitalOutput.setDeviceSerialNumber(this.serial);
-    this.digitalOutput.setChannel(this.channel);
-    this.digitalOutput.open(5000).catch(function (err) {
-      console.error("Error during open:", err);
-    });
-    this.open = true;
-    this.activated = false;
-  }
-
-  activate() {
-    if (this.open) {
-      this.digitalOutput.setDutyCycle(1).catch(function (err) {
-        console.error("Error during open:", err);
-      });
-      this.activated = true;
-      return true;
-    }
-    return false;
-  }
-
-  deactivate() {
-    if (this.open) {
-      this.digitalOutput.setDutyCycle(0).catch(function (err) {
-        console.error("Error during open:", err);
-      });
-      this.activated = false;
-      return true;
-    }
-    return false;
-  }
-
-  close() {
-    this.digitalOutput.close();
-    this.open = false;
-    return true;
-  }
-
-  getSerial() {
-    return this.serial;
-  }
-
-  getChannel() {
-    return this.channel;
-  }
-
-  isActivated() {
-    return this.activated;
-  }
-
-  isOpen() {
-    return this.open;
-  }
-}
+   setTimeout(function () {
+     //Close your Phidgets once the program is done.
+     digitalOutput15.close();
+     process.exit(0);
+   }, 5000);
+ });
--- src/renderer.js
+++ src/renderer.js
@@ -1,143 +1,135 @@
-const { ipcRenderer } = require('electron');
+// const { ipcRenderer } = require('electron');
 
- let data = {
-    "loading": false,
-    "allowed_phidgets": {
-        "257037" : "A",
-        "312483" : "B",
-        "478550" : "C",
-    },
-    "projecten": {
-      1: {
-        "titel": "Keizersbrug",
-        "omschrijving": "Woningstichting realiseerde 11 appartementen en 1 bedrijfsruimte op de begane grond.",
-        "architect": "Kapitein Jacobs Kapitein te Den Helder",
-        "ontwikkelaar": "Woningstichting Den Helder",
-        "aannemer": "aannemingsbedrijf Dozy BV",
-        "gebruiker": "n.v.t",
-        "planning": "juni 2014 opgeleverd",
-        "informatie": "Neem voor meer informatie contact op met Woningstichting Den Helder (0223 677677) of bekijk de website www.wsdh.nl ",
-        "plaatjes": {
-            "plaatje": "img_1.jpg",
-            "plaatje": "img_1.jpg",
-        },
-        "phidget": {
-            "257037" : [
-                15, 6
-            ],
-        },
-      },
-      2: {
-        "titel": "School 7 bibliotheek Den Helder",
-        "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
-        "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
-        "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
-        "aannemer": "aannemingsbedrijf Dozy BV",
-        "gebruiker": "Kopgroep bibliotheken Den Helder",
-        "planning": "april 2016 opgeleverd",
-        "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
-        "plaatjes": {
-            "plaatje": "img_2.jpg"
-        },
-        "phidget": {
-            "312483" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-        },
-      },
-      3: {
-        "titel": "School 7 bibliotheek Den Helder",
-        "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
-        "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
-        "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
-        "aannemer": "aannemingsbedrijf Dozy BV",
-        "gebruiker": "Kopgroep bibliotheken Den Helder",
-        "planning": "april 2016 opgeleverd",
-        "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
-        "plaatjes": {
-            "plaatje": "img_2.jpg"
-        },
-        "phidget": {
-            "478550" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-        },
-      },
-      4: {
-        "titel": "School 7 bibliotheek Den Helder",
-        "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
-        "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
-        "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
-        "aannemer": "aannemingsbedrijf Dozy BV",
-        "gebruiker": "Kopgroep bibliotheken Den Helder",
-        "planning": "april 2016 opgeleverd",
-        "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
-        "plaatjes": {
-            "plaatje": "img_2.jpg"
-        },
-        "phidget": {
-            "257037" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-        },
-      },
-      5: {
-        "titel": "School 7 bibliotheek Den Helder",
-        "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
-        "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
-        "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
-        "aannemer": "aannemingsbedrijf Dozy BV",
-        "gebruiker": "Kopgroep bibliotheken Den Helder",
-        "planning": "april 2016 opgeleverd",
-        "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
-        "plaatjes": {
-            "plaatje": "img_2.jpg"
-        },
-        "phidget": {
-            "312483" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-            "478550" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-            "257037" : [
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
-            ],
-        },
-      },
-    }
-  }
+//  let data = {
+//     "loading": false,
+//     "allowed_phidgets": {
+//         "257037" : "A",
+//         "312483" : "B",
+//         "478550" : "C",
+//     },
+//     "projecten": {
+//       1: {
+//         "titel": "Keizersbrug",
+//         "omschrijving": "Woningstichting realiseerde 11 appartementen en 1 bedrijfsruimte op de begane grond.",
+//         "architect": "Kapitein Jacobs Kapitein te Den Helder",
+//         "ontwikkelaar": "Woningstichting Den Helder",
+//         "aannemer": "aannemingsbedrijf Dozy BV",
+//         "gebruiker": "n.v.t",
+//         "planning": "juni 2014 opgeleverd",
+//         "informatie": "Neem voor meer informatie contact op met Woningstichting Den Helder (0223 677677) of bekijk de website www.wsdh.nl ",
+//         "plaatjes": {
+//             "plaatje": "img_1.jpg",
+//             "plaatje": "img_1.jpg",
+//         },
+//         "phidget": {
+//             "257037" : [
+//                 15, 6
+//             ],
+//         },
+//       },
+//       2: {
+//         "titel": "School 7 bibliotheek Den Helder",
+//         "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
+//         "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
+//         "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
+//         "aannemer": "aannemingsbedrijf Dozy BV",
+//         "gebruiker": "Kopgroep bibliotheken Den Helder",
+//         "planning": "april 2016 opgeleverd",
+//         "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
+//         "plaatjes": {
+//             "plaatje": "img_2.jpg"
+//         },
+//         "phidget": {
+//             "312483" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//         },
+//       },
+//       3: {
+//         "titel": "School 7 bibliotheek Den Helder",
+//         "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
+//         "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
+//         "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
+//         "aannemer": "aannemingsbedrijf Dozy BV",
+//         "gebruiker": "Kopgroep bibliotheken Den Helder",
+//         "planning": "april 2016 opgeleverd",
+//         "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
+//         "plaatjes": {
+//             "plaatje": "img_2.jpg"
+//         },
+//         "phidget": {
+//             "478550" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//         },
+//       },
+//       4: {
+//         "titel": "School 7 bibliotheek Den Helder",
+//         "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
+//         "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
+//         "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
+//         "aannemer": "aannemingsbedrijf Dozy BV",
+//         "gebruiker": "Kopgroep bibliotheken Den Helder",
+//         "planning": "april 2016 opgeleverd",
+//         "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
+//         "plaatjes": {
+//             "plaatje": "img_2.jpg"
+//         },
+//         "phidget": {
+//             "257037" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//         },
+//       },
+//       5: {
+//         "titel": "School 7 bibliotheek Den Helder",
+//         "omschrijving": "Renovatie gemeentelijk monument in combinatie met nieuwbouw. School 7 heeft de Public Library of the Year Award 2018 gewonnen.",
+//         "architect": "Drost en van Veen te Rotterdam i.s.m. Mars interieur te Rotterdam",
+//         "ontwikkelaar": "Woningstichting Den Helder i.s.m. Zeestad",
+//         "aannemer": "aannemingsbedrijf Dozy BV",
+//         "gebruiker": "Kopgroep bibliotheken Den Helder",
+//         "planning": "april 2016 opgeleverd",
+//         "informatie": "www.wsdh.nl en www.kopgroepbibliotheken.nl",
+//         "plaatjes": {
+//             "plaatje": "img_2.jpg"
+//         },
+//         "phidget": {
+//             "312483" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//             "478550" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//             "257037" : [
+//                 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+//             ],
+//         },
+//       },
+//     }
+//   }
 
-// Call the register phidgets function from the main process
-ipcRenderer.send('register-phidgets', data.projecten);
-
-// Listen for the response from register-phidgets
-ipcRenderer.on('register-phidgets-response', (event, data) => {
-    console.log(data);
-});
-
-ipcRenderer.on('error-response', (event, error) => {
-    console.log(error);
-});
+//   ipcRenderer.on('error-response', (event, error) => {
+//     console.log(error);
+// });
 
 
 
-document.getElementById('button1').addEventListener('click', function(){
-    ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
-});
+// document.getElementById('button1').addEventListener('click', function(){
+//     ipcRenderer.send('test');
+// });
 
-document.getElementById('button2').addEventListener('click', function(){
-    ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
-});
+// document.getElementById('button2').addEventListener('click', function(){
+//     ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
+// });
 
-document.getElementById('button3').addEventListener('click', function(){
-    ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
-});
+// document.getElementById('button3').addEventListener('click', function(){
+//     ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
+// });
 
-document.getElementById('button4').addEventListener('click', function(){
-    ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
-});
+// document.getElementById('button4').addEventListener('click', function(){
+//     ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
+// });
 
-document.getElementById('button5').addEventListener('click', function(){
-    ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
-});
+// document.getElementById('button5').addEventListener('click', function(){
+//     ipcRenderer.send('turn-on-lights', data.projecten[this.dataset.project]);
+// });
